46d2d106d00390393c52887c93bd51309fc8d9d8
Pavel Slabikov <pslabikov@mail.ru>
2021-02-24 16:42:04 +0500

    Entry point for tracert task

---
+++ tracert_as/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+from tracert_as.tracer import Tracer
+
+
+def prepare_args():
+    parser.add_argument("-t", "--ttk", type=int, metavar="SEC", help="Время жизни пакета", default=15)
+    parser.add_argument("ip_address", type=str, help="IP адрес")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    prepare_args()
+    args = parser.parse_args()
+    tracer = Tracer()
+    try:
+        tracer.trace_address(args.ip, args.ttk)
+    except Exception as e:
+        print(e)
---
+++ tracert_as/tracer.py
@@ -0,0 +1,3 @@
+class Tracer:
+    def trace_address(self, ip: str, ttk: int):
+        pass
